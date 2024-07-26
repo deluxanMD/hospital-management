6960d32dba33999dcd958783b7dc20b08815c209
Deluxan Mariathasan <deluxan.m@aeturnum.com>
2024-07-26 21:21:56 +0530

    fake api

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeSlice from "global/theme/theme.slice";
 import sidebarSlice from "global/sidebar/sidebar.slice";
+import { pokemonApi } from "api/fake/fake-api";
 
 export const store = configureStore({
   reducer: {
     theme: themeSlice,
     sidebar: sidebarSlice,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
 
 if (window.Cypress) {
